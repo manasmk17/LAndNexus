fbd4ecd6922cf16a68da93c5c0c74bacf93730b3
officialexperti <40721059-officialexperti@users.noreply.replit.com>
2025-06-16 20:13:20 +0000

    Restored to '5cbff6614fa06703866e53ea17f43222999fb296'
    Replit-Restored-To: 5cbff6614fa06703866e53ea17f43222999fb296

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,6 @@ import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 
-
 import { useState, useEffect } from "react";
 
 import Navbar from "@/components/layout/navbar";

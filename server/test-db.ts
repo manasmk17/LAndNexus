4b390b04ca021dd85b1049b119136ed68571e078
sevilsariyeva <sevlsariyeva00@gmail.com>
2025-07-14 21:17:55 +0400

    fixed some problems

--- server/test-db.ts
+++ server/test-db.ts
@@ -1,13 +1,15 @@
 import { Client } from 'pg';
 
 const client = new Client({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'postgres',
-  password: 'password',
-  port: 5432,
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
+  port: Number(process.env.PGPORT),
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
-
 
 async function test() {
   try {
